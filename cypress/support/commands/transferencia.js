5ffa614a6fff913451411cfa0b34796a67d712a2
LucasConfortini <lucasconfortini1@gmail.com>
2026-07-03 06:25:58 -0300

    Implementando comandos customizaveis para Login, Transferencias e Commons

---
+++ cypress/support/commands/transferencia.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('realizarTransferencia', (contaOrigem, contaDestino, valor) => {
+    cy.selecionarOpcaoCombobox('conta-origem', contaOrigem)
+    cy.selecionarOpcaoCombobox('conta-destino', contaDestino)
+    cy.get('#valor').click().type(valor)
+    cy.contains('button', 'Transferir').click()
+
+})
